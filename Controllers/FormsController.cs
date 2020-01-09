ce6a38f473740d0046e9bd852e40250953c57910
Daniel Moorhouse <danmoorhouse80@gmail.com>
2020-01-09 10:18:01 +0000

    first commit

---
+++ Controllers/FormsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using FNCORCL2.Oracle;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace FNCORCL2.Controllers
+{
+    public class FormsController : Controller
+    {
+        private readonly OracleConn _orcl;
+
+        public FormsController(OracleConn orcl)
+        {
+            _orcl = orcl;
+        }
+        public IActionResult OpitoGWO()
+        {
+            return View();
+        }
+        //public async Task LoadData()
+        //{
+        //try
+        //    {
+                
+        //    }
+        //}
+        public DataTable OpitoGWO(string personCode, string unitInstanceCode, string ucaloccCode, string certificateExpiryDate,
+            string newCertStartDate, string refreshCertEndDate, string surname, string forename, string createdBy, 
+            string createdDate, string updatedBy, string updatedDate )
+        {
+            using (OracleConnection cn = new OracleConnection(DatabaseHelper.GetConnectionString()))
+            {
+                OracleDataAdapter da = new OracleDataAdapter();
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = cn;
+                cmd.InitialLONGFetchSize = 1000;
+                cmd.CommandText = DatabaseHelper.GetDBOwner() + "populate_opito_certificates";//stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("PERSON_CODE", OracleDbType.Int32).Value = personCode;
+                cmd.Parameters.Add("UI_CODE", OracleDbType.Char).Value = unitInstanceCode;
+                cmd.Parameters.Add("UCALOCC_CODE", OracleDbType.Char).Value = ucaloccCode;
+                cmd.Parameters.Add("CERTIFICATE_EXPIRY_DATE", OracleDbType.Date).Value = certificateExpiryDate;
+                cmd.Parameters.Add("NEW_CERT_START_DATE", OracleDbType.Date).Value = newCertStartDate;
+                cmd.Parameters.Add("REFRESH_CERT_END_DATE", OracleDbType.Date).Value = refreshCertEndDate;
+                cmd.Parameters.Add("SURMANE", OracleDbType.Char).Value = surname;
+                cmd.Parameters.Add("FORENAME", OracleDbType.Char).Value = forename;
+                cmd.Parameters.Add("CREATED_BY", OracleDbType.Char).Value = createdBy;
+                cmd.Parameters.Add("CREATED_DATE", OracleDbType.Date).Value = createdDate;
+                cmd.Parameters.Add("UPDATED_BY", OracleDbType.Char).Value = updatedBy;
+                cmd.Parameters.Add("UPDATED_DATE", OracleDbType.Char).Value = updatedDate;
+                da.SelectCommand = cmd;
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+    }
+
+    public class LoadData
+    {
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using Microsoft.Extensions.Configuration;
+//using FNCORCL2.Data;
+
+namespace FNCORCL2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var con_string = Configuration.GetConnectionString("DefaultConnection");
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseOracle(con_string));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Forms}/{action=OpitoGWO}/{id?}");
+            });
+
+     
+        }
+    }
+}
